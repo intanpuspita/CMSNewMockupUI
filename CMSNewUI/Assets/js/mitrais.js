21a0422c20720bb3fbca09ac2ed719aa564b5895
Intan Puspitasari <intanpusss@gmail.com>
2016-11-22 12:07:36 +0700

    Initial Commit

---
+++ CMSNewUI/Assets/js/mitrais.js
@@ -0,0 +1,45 @@
+var $table = $('#table');
+var mydata = 
+[
+    {
+        "comp_item": "Computing Foundations",
+        "last_result": "Working",
+        "self_review": "Working",
+        "champ_review": "...",
+        "spv_review": "..."
+    },
+    {
+        "comp_item": "Software Construction",
+        "last_result": "Working",
+        "self_review": "Working",
+        "champ_review": "...",
+        "spv_review": "..."
+    },
+    {
+        "comp_item": "Software Design",
+        "last_result": "Working",
+        "self_review": "Working",
+        "champ_review": "...",
+        "spv_review": "..."
+    }
+];
+
+$(function () {
+    var placeholder = "&#xf002 Jump to a competency, project, staff or search";
+    $(".select2").select2({
+      placeholder: placeholder,
+      allowClear: true,
+      escapeMarkup: function(m) { 
+         return m; 
+      }
+    });
+    
+    $('#table').bootstrapTable({
+        data: mydata
+    });
+    console.log(mydata);
+});
+
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
---
+++ CMSNewUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CMSNewUI.MvcApplication" Language="C#" %>
